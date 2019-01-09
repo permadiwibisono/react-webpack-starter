86030a638fe988256abd6f1564df9f0e937a9ec2
Permadi Wibisono <mbapewe@gmail.com>
2019-01-09 09:26:59 +0700

    Fixing and modify webpack config for dev following webpack-dev-server's new version

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -1,28 +1,40 @@
 var path = require('./path.js');
 var webpack=require('webpack');
 const config = {
-    entry:path.entry,
-    module:{
-        rules:[
-            {test:/\.json$/,loader:"json-loader"},
-            {test:/\.(eot?.+|ttf?.+|otf?.+|woff?.+|woff2?.+)$/,loader:"file-loader"},
-            {test:/\.(png|svg|jpg|gif)$/,loader:"url-loader?limit=10000"},
-            {test:/\.jsx?$/, exclude:/node_modules/, loader:"babel-loader"}
-        ]
-    },
-    resolve:{
-        extensions:['.js','.jsx']
-    },
-    plugins:[
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "vendor",
-            filename: path.vendor.filename,
-            minChunks: function (module) {
-                // this assumes your vendor imports exist in the node_modules directory
-                return module.context && module.context.includes("node_modules");
-            }
-        })
-    ],
-    devtool:'source-map'
+  entry:path.entry,
+  module: {
+      rules: [
+        { test:/\.json$/,loader:"json-loader" },
+        { test:/\.(eot?.+|ttf?.+|otf?.+|woff?.+|woff2?.+)$/,loader:"file-loader" },
+        { test:/\.(png|svg|jpg|gif)$/,loader:"url-loader?limit=10000" },
+        { test:/\.jsx?$/, exclude:/node_modules/, loader:"babel-loader" }
+      ]
+  },
+  resolve:{
+    extensions:['.js','.jsx']
+  },
+  plugins:[
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "vendor",
+    //   filename: path.vendor.filename,
+    //   minChunks: function (module) {
+    //     // this assumes your vendor imports exist in the node_modules directory
+    //     return module.context && module.context.includes("node_modules");
+    //   }
+    // })
+  ],
+  optimization:{
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          filename: path.vendor.filename,
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  devtool:'source-map'
 }
 module.exports = config;
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -4,49 +4,57 @@ const merge=require('webpack-merge');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const InterpolateHtmlPlugin=require('interpolate-html-plugin');
 const base = require('./webpack.base.config.js');
+const autoprefixer = require('autoprefixer');
 
 // definePlugin takes raw strings and inserts them, so you can put strings of JS if you want.
 const definePlugin = new webpack.DefinePlugin({
-    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-    __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+  __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
 });
 
-const config={
-    output:path.development.output,
-    module:{
-        rules:[
-            {test:/\.css$/,use:[
-                'style-loader',
-                'css-loader',
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                      ident: 'postcss',
-                      plugins: [
-                          require('autoprefixer')
-                      ]
-                    }
-                }
-            ]}
+// console.log("PATHS: ", path);
+
+const config = {
+  mode: 'development',
+  output:path.development.output,
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                autoprefixer
+              ]
+            }
+          }
         ]
-    },
-    plugins:[
-        new InterpolateHtmlPlugin({
-            'PUBLIC_URL':`http://localhost:${path.development.port}`
-        }),
+      }
+    ]
+  },
+  plugins:[
+    // new InterpolateHtmlPlugin({ //Deprecated...
+    //   'PUBLIC_URL':`http://localhost:${path.development.port}`
+    // }),
 
-        new HtmlWebpackPlugin({
-            template:path.development.templateHtml,
-            inject:true
-        }),
+    new HtmlWebpackPlugin({
+      template:path.development.templateHtml,
+      inject:true
+    }),
 
-        new webpack.HotModuleReplacementPlugin(),
-        
-        definePlugin
-    ],
-    devServer:{
-        contentBase:'./public',
-        port:path.development.port
-    }
+    new webpack.HotModuleReplacementPlugin(),
+
+    definePlugin
+  ],
+  devServer:{
+    contentBase: './public',
+    port: path.development.port
+  }
 }
+// console.log('HASIL: ', merge(base, config))
 module.exports=merge(base,config);
