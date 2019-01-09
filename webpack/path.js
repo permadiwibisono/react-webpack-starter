1e9bbbec62bfe2bfac4a5970442d38dc0b77a742
Permadi Wibisono <mbapewe@gmail.com>
2019-01-09 09:26:03 +0700

    Fixing indent in webpack/path.js

--- webpack/path.js
+++ webpack/path.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports={
-    entry:path.resolve('src/index.js'),
-    production:{
-        output:{
-            path:path.resolve('build/assets'),
-            publicPath:'/assets/',
-            filename:'bundles.[chunkhash:8].js'
-        },
-        rootPath:path.resolve('/'),
-        sourcePath:path.resolve('src'),
-        staticPath:path.resolve('public'),
-        buildPath:path.resolve('build'),
-        templateHtml:path.resolve('public/index.html'),
+  entry:path.resolve(__dirname, '../src/index.js'),
+  production:{
+    output:{
+      path:path.resolve('build/assets'),
+      publicPath:'/assets/',
+      filename:'bundles.[chunkhash:8].js'
     },
-    development:{
-        output:{
-            path:path.resolve('public'),
-            publicPath:'/',
-            filename:'bundles.[chunkhash:8].js'
-        },
-        templateHtml:path.resolve('public/index.html'),
-        port:'9000',
+    rootPath:path.resolve('/'),
+    sourcePath:path.resolve('src'),
+    staticPath:path.resolve('public'),
+    buildPath:path.resolve('build'),
+    templateHtml:path.resolve('public/index.html'),
+  },
+  development:{
+    output:{
+      path: path.resolve(__dirname, '../dist'),
+      publicPath: '/',
+      filename: 'bundles.[hash:8].js'
     },
-    vendor:{
-        filename:'vendors.[hash:8].js'
-    }
+    templateHtml: path.resolve(__dirname, '../public/index.html'),
+    port: 9000,
+  },
+  vendor:{
+    filename: '[name].[hash:8].js'
+  }
 }
